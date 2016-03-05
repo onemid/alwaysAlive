a8c9c96747e8c175a1abd24fcb7b75e8c34c26a5
Gong, Yi-Jhong <onemid85@gmail.com>
2016-03-05 16:47:55 +0800

    Update alwaysAlive.sh
    Support the parameter.

--- alwaysAlive.sh
+++ alwaysAlive.sh
@@ -4,18 +4,23 @@
 PATH=/bin:/sbin:/usr/bin:/usr/sbin:/usr/local/bin:/usr/local/sbin:~/bin
 export PATH
 
-#Set your monitor target here:
-target=nginx
+for target in "$@"
+do
 
-echo "We are now checking the service status"
+echo "We are now checking ${target} service status"
 
 log=/dev/shm/servic_${target}_check.txt
 service ${target} status>${log}
 testlog=$(grep "Active: active" ${log})
-
 if [ "${testlog}" != "" ]; then
     echo "${target} Service is working"
 else
     echo "We detect the service down, now restarting ${target}"
     systemctl restart ${target}
 fi
+
+echo "-------------------------------"
+
+done
+
+echo "FINISHED THE CHECK ROUTIN
